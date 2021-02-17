9b6ea29ca3020c3d220b0323ed32c1797adb4ee8
abcjamesjordan <abcjamesjordan@gmail.com>
2021-02-16 17:16:38 -0700

    two sum

---
+++ Two Sum.py
@@ -0,0 +1,9 @@
+nums = [2,7,11,15]
+target = 9
+answer_dict = {}
+
+    for index, num in enumerate(nums):
+        try:
+            return ((answer_dict[target-num], index))
+        except:
+            answer_dict[num] = index
